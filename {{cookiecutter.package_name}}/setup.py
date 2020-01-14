234db0b6882855073388e12c77cd735fda663037
Niklas Meinzer <github@niklas-meinzer.de>
2020-01-14 09:36:39 +0100

    Update setup.py to python 3 and make it black compliant

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -8,36 +8,30 @@ kwargs = {
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 
 setuptools.setup(
     name="{{ cookiecutter.package_name }}",
     version="{{ cookiecutter.package_version }}",
-
     author="{{ cookiecutter.author_name }}",
     author_email="{{ cookiecutter.author_email }}",
-
     description="{{ cookiecutter.package_description }}",
-    long_description=open('README.rst').read(),
-
+    long_description=open("README.rst").read(),
     packages=setuptools.find_packages(),
-
     install_requires=install_requires,
-
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Healthcare Industry',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Healthcare Industry",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-
     entry_points={
-        'chemocompile_plugin': [
-            '{{ cookiecutter.package_name }}={{ cookiecutter.package_slug }}:register_plugin'
+        "chemocompile_plugin": [
+            "{{ cookiecutter.package_name }}={{ cookiecutter.package_slug }}:register_plugin"
         ],
     },
     **kwargs
