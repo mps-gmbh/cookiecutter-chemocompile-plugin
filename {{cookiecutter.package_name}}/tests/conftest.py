68b88690b339eec4bbdabcb908552eda3e42b9e9
Niklas Meinzer <github@niklas-meinzer.de>
2022-03-01 11:15:21 +0100

    Add conftest.py

---
+++ {{cookiecutter.package_name}}/tests/conftest.py
@@ -0,0 +1,9 @@
+pytest_plugins = ["chemocompile.tests.conftest"]
+
+
+def pytest_modify_app_settings(settings):
+    """
+    Modify the settings with settings needed by all tests for this plugin.
+    """
+    pass
+
